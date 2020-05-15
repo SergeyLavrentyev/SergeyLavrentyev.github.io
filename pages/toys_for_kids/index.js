03a06cf3bf9ae4877a3259183912b1fd184c5722
Sergey <53293773+SergeyLavrentyev@users.noreply.github.com>
2020-05-15 16:34:34 +0400

    init

---
+++ pages/toys_for_kids/index.js
@@ -0,0 +1,1 @@
+"use strict";document.querySelector(".toys-form__range").oninput=function(){this.style.background="linear-gradient(to right, #C29974 0%, #C29974 "+this.value/5+"%, rgba(0, 0, 0, 0.5) "+this.value/5+"%, rgba(0, 0, 0, 0.5) 100%)"};
